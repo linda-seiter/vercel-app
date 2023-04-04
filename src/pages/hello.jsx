07fb40b6feb0cc34f19ddd7f51fd7a59dbc700da
Linda Seiter <linseiter@gmail.com>
2023-04-04 11:13:49 -0400

    non-nested sandpack page

--- src/pages/hello.jsx
+++ src/pages/hello.jsx
@@ -1,3 +1,19 @@
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackTests,
+  SandpackCodeEditor,
+  SandpackConsole,
+  SandpackPreview,
+} from "@codesandbox/sandpack-react";
+
 export default function Hello() {
-  return <div>Hello!</div>;
+  return (
+    <SandpackProvider>
+      <SandpackLayout>
+        <SandpackCodeEditor showLineNumbers showTabs />
+        <SandpackConsole resetOnPreviewRestart="true" standalone="true" />
+      </SandpackLayout>
+    </SandpackProvider>
+  );
 }
