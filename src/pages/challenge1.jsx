3d1fb74012f3d7a93c6cd36c5bca4691376918d0
Linda Seiter <linseiter@gmail.com>
2023-04-04 11:22:41 -0400

    add challenge1 problem

--- src/pages/challenge1.jsx
+++ src/pages/challenge1.jsx
@@ -7,9 +7,17 @@ import {
   SandpackPreview,
 } from "@codesandbox/sandpack-react";
 
-export default function Hello() {
+import code from "./challenge1/index.js?raw";
+
+const files = {
+  "/index.js": {
+    code: code,
+  },
+};
+
+export default function Playground() {
   return (
-    <SandpackProvider>
+    <SandpackProvider files={files}>
       <SandpackLayout>
         <SandpackCodeEditor showLineNumbers showTabs />
         <SandpackConsole resetOnPreviewRestart="true" standalone="true" />
